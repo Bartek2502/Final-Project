9fd61472e228b695d8e89d21590058630f6d8890
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-02-11 10:27:12 +0000

    Added selection change to the combo box, added description class

--- Final Project/MainWindow.xaml.cs
+++ Final Project/MainWindow.xaml.cs
@@ -32,22 +32,28 @@ namespace Final_Project
             string[] genres = { "All", "Platformer", "RPG", "Horror", "Puzzle" };
             cbxGenre.ItemsSource = genres;
 
+            //Platformer Games
             Games g1 = new Platformer() { Name = "Celeste", RealeaseYear = 2018, Score = 9.5 };
             Games g2 = new Platformer() { Name = "Super Mario Odyssey", RealeaseYear = 2017, Score = 8.0 };
             Games g3 = new Platformer() { Name = "Crash 4", RealeaseYear = 2020, Score = 9.0 };
             Games g4 = new Platformer() { Name = "A Hat in Time", RealeaseYear = 2017, Score = 7.6 };
 
+            //Role Playing Games
             Games g5 = new RPG() { Name = "Final Fantasy VII", RealeaseYear = 1996, Score = 9.0};
             Games g6 = new RPG() { Name = "Persona 5", RealeaseYear = 2016, Score = 9.9};
             Games g7 = new RPG() { Name = "The Witcher 3", RealeaseYear = 2015, Score = 8.3 };
 
+            //Horror Games
             Games g8 = new Horror() { Name = "Outlast", RealeaseYear = 2014, Score = 6.3 };
             Games g9 = new Horror() { Name = "Phasmophobia", RealeaseYear = 2020, Score = 7.8 };
             Games g10 = new Horror() { Name = "Resident Evil VILLAGE", RealeaseYear = 2021, Score = 9.0 };
 
+            //Puzzle Games
             Games g11 = new Puzzle() { Name = "Bejeweled 3", RealeaseYear = 2010, Score = 8.2 };
             Games g12 = new Puzzle() { Name = "Candy Crush", RealeaseYear = 2012, Score = 2.5 };
 
+
+            //Adding all the games to the list
             allGames.Add(g1);
             allGames.Add(g2);
             allGames.Add(g3);
@@ -61,12 +67,72 @@ namespace Final_Project
             allGames.Add(g11);
             allGames.Add(g12);
 
+            //Adding the list of games to the list box
             lbxGames.ItemsSource = allGames;
 
 
 
 
 
+
+        }
+
+        private void CbxGenre_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedGenre = cbxGenre.SelectedItem as string;
+
+            List<Games> filteredList = new List<Games>();
+
+            switch (selectedGenre)
+            {
+                case "All":
+                    lbxGames.ItemsSource = allGames;
+                    break;
+
+                case "Platformer":
+
+                    foreach (Games game in allGames)
+                    {
+                        if (game is Platformer)
+                            filteredList.Add(game);
+                    }
+                    lbxGames.ItemsSource = filteredList;
+                    break;
+
+                case "RPG":
+
+                    foreach (Games game in allGames)
+                    {
+                        if (game is RPG)
+                            filteredList.Add(game);
+                    }
+                    lbxGames.ItemsSource = filteredList;
+                    break;
+
+                case "Horror":
+
+                    foreach (Games game in allGames)
+                    {
+                        if (game is Horror)
+                            filteredList.Add(game);
+                    }
+                    lbxGames.ItemsSource = filteredList;
+                    break;
+
+                case "Puzzle":
+                    foreach (Games game in allGames)
+                    {
+                        if (game is Puzzle)
+                            filteredList.Add(game);
+                    }
+                    lbxGames.ItemsSource = filteredList;
+                    break;
+
+            }
+        }
+
+        private void LbxGames_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
 
         }
     }
