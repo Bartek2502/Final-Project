8ca4b1ef16dc87b5a71bee8faef62e2ce083cc25
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-02-11 09:07:41 +0000

    Add project files.

---
+++ Final Project/Games.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    class Games
+    {
+        public string Name { get; set; }
+        public double Score { get; set; }
+        public DateTime RealeaseDate { get; set; }
+
+        public Games()
+        {
+
+        }
+        public Games(string name, double score, DateTime realeaseDate)
+        {
+            name = Name;
+            score = Score;
+            realeaseDate = RealeaseDate;   
+        }
+
+    }
+}
