cb76b051fa4fb4c842c0c9b1f6920bdacadb86f5
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-04-28 16:49:29 +0100

    Created a new class that will act as a Json class

---
+++ Final Project/OrderNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    public class OrderNumber
+    {
+        public string RandomOrderNumber { get; set; }
+
+        public OrderNumber(string randomOrderNumber)
+        {
+            randomOrderNumber = RandomOrderNumber;
+        }
+
+        public OrderNumber() : this("Unknown") { }
+
+        public override string ToString()
+        {
+            return string.Format("[{0}] {1}", this.GetType().Name.ToUpper(), RandomOrderNumber);
+        }
+    }
+}
