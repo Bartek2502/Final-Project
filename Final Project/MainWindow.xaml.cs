74ee8011aad6cee13cfe1cb8f45a1cf0bb65ab97
Bartlomiej Sajdok <S00196895@mail.itsligo.ie>
2021-04-29 11:26:43 +0100

    Moved some stuff from the window loaded method to its own method

--- Final Project/MainWindow.xaml.cs
+++ Final Project/MainWindow.xaml.cs
@@ -36,6 +36,34 @@ namespace Final_Project
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //Calls a method to add all of the Games and Genres to the "Game Descriptions" section
+            AddGamesAndGenres();
+
+            var query = from gc in db.GameCompanies
+                        orderby gc.CompanyName
+                        select gc;
+
+            var results = query.ToList();
+
+            GameCompaniesBox.ItemsSource = results;
+
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += dt_Tick;
+            dt.Start();
+           
+        }
+
+        void dt_Tick(object sender, EventArgs e)
+        {
+            int minute = timeLimit.Minute;
+            int second = timeLimit.Second;
+            timeLimit = timeLimit.AddSeconds(-1);
+
+            tblkSpecialTimer.Text = timeLimit.ToString($"{minute} : {second}");
+        }
+        private void AddGamesAndGenres()
+        {
             //Genre names added to the combo box
             string[] genres = { "All", "Platformer", "RPG", "Horror", "Puzzle" };
             cbxGenre.ItemsSource = genres;
@@ -43,10 +71,10 @@ namespace Final_Project
             string[] rating = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" };
             cbxRate.ItemsSource = rating;
             //Platformer Games
-            Games g1 = new Platformer() { Name = "Celeste", Pegi = 7, GameImage ="/images/celeste.jpg" };
-            Games g2 = new Platformer() { Name = "Super Mario Odyssey", Pegi = 3, GameImage ="/images/odyssey.jpg" };
-            Games g3 = new Platformer() { Name = "Crash 4", Pegi = 12, GameImage ="/images/crash.jpg" };
-            Games g4 = new Platformer() { Name = "A Hat in Time", Pegi = 7, GameImage ="/images/hat.jpg" };
+            Games g1 = new Platformer() { Name = "Celeste", Pegi = 7, GameImage = "/images/celeste.jpg" };
+            Games g2 = new Platformer() { Name = "Super Mario Odyssey", Pegi = 3, GameImage = "/images/odyssey.jpg" };
+            Games g3 = new Platformer() { Name = "Crash 4", Pegi = 12, GameImage = "/images/crash.jpg" };
+            Games g4 = new Platformer() { Name = "A Hat in Time", Pegi = 7, GameImage = "/images/hat.jpg" };
 
             //Role Playing Games
             Games g5 = new RPG() { Name = "Final Fantasy VII", Pegi = 12, GameImage = "/images/ff7.jpg" };
@@ -68,7 +96,7 @@ namespace Final_Project
                 Synopsis = "Celeste is a platform game in which players control a girl named Madeline as she makes her way up a mountain while avoiding various deadly obstacles. ",
                 ReleaseYear = 2018,
                 Score = 9.5,
-                
+
             };
 
             Description d2 = new Description()
@@ -76,7 +104,7 @@ namespace Final_Project
                 Synopsis = "Super Mario Odyssey is a platform game in which players control Mario as he travels across many different worlds, known as Kingdoms within the game, on the hat-shaped ship Odyssey",
                 ReleaseYear = 2017,
                 Score = 8.0,
-               
+
             };
 
             Description d3 = new Description()
@@ -84,7 +112,7 @@ namespace Final_Project
                 Synopsis = "It's About Time places players in control of five playable characters as they traverse multiple levels, each filled with enemies, crates, Wumpa fruit and hazards, with the objective being to get from the start point to the goal as with the original trilogy.",
                 ReleaseYear = 2020,
                 Score = 9.0,
-                
+
             };
 
             Description d4 = new Description()
@@ -92,7 +120,7 @@ namespace Final_Project
                 Synopsis = "A Hat in Time is a platform action-adventure game set in an open world environment and played from a third-person perspective.",
                 ReleaseYear = 2017,
                 Score = 7.9,
-                
+
             };
 
             Description d5 = new Description()
@@ -100,7 +128,7 @@ namespace Final_Project
                 Synopsis = "The gameplay of Final Fantasy VII is mostly comparable to earlier Final Fantasy games and Japanese role-playing games. The game features three modes of play: the world map, the field, and the battle screen.",
                 ReleaseYear = 1997,
                 Score = 9.7,
-                
+
             };
 
             Description d6 = new Description()
@@ -108,7 +136,7 @@ namespace Final_Project
                 Synopsis = "Persona 5 is a role-playing video game where the player takes on the role of a male high school student, codenamed Joker, who lives out a single year while attending school in modern-day Tokyo.",
                 ReleaseYear = 2016,
                 Score = 9.9,
-                
+
             };
 
             Description d7 = new Description()
@@ -116,7 +144,7 @@ namespace Final_Project
                 Synopsis = "The Witcher 3: Wild Hunt is an action role-playing game with a third-person perspective. Players control Geralt of Rivia, a monster slayer known as a Witcher.",
                 ReleaseYear = 2015,
                 Score = 8.3,
-                
+
             };
 
             Description d8 = new Description()
@@ -124,14 +152,14 @@ namespace Final_Project
                 Synopsis = "In Outlast, the player assumes the role of investigative journalist Miles Upshur, as he navigates a dilapidated psychiatric hospital in Leadville, Colorado that is overrun by homicidal patients.",
                 ReleaseYear = 2014,
                 Score = 5.5,
-                
+
             };
             Description d9 = new Description()
             {
                 Synopsis = "The player takes control of one member from a group of up to four players, in the roles of urban exploring ghost hunters, who are contracted to deal with ghosts inhabiting different abandoned facilities such as homes, schools, prisons and hospitals.",
                 ReleaseYear = 2020,
                 Score = 8.2,
-                
+
             };
 
             Description d10 = new Description()
@@ -139,7 +167,7 @@ namespace Final_Project
                 Synopsis = "Like Resident Evil 7 (2017), Resident Evil Village uses a first-person perspective. The inventory management system is similar to that of Resident Evil 4 (2005), featuring a briefcase.",
                 ReleaseYear = 2021,
                 Score = 9.0,
-                
+
             };
 
             Description d11 = new Description()
@@ -147,7 +175,7 @@ namespace Final_Project
                 Synopsis = "The core objective of Bejeweled 3 is similar to the previous installments in the series. Players swap any on-screen gem with an adjacent one to form chains of three or more gems of the same color.",
                 ReleaseYear = 2010,
                 Score = 7.8,
-                
+
             };
 
             Description d12 = new Description()
@@ -155,7 +183,7 @@ namespace Final_Project
                 Synopsis = "Candy Crush Saga is a match three game, where the core gameplay is based on swapping two adjacent candies among several on the gameboard as to make a row or column of at least 3 matching-colored candies.",
                 ReleaseYear = 2012,
                 Score = 2.4,
-                
+
             };
 
 
@@ -190,63 +218,6 @@ namespace Final_Project
 
             //Adding the list of games to the list box
             lbxGames.ItemsSource = allGames;
-
-
-
-            /*
-            * ##################################################
-            * ##################################################
-            * ##################################################
-            */
-            var query = from gc in db.GameCompanies
-                        orderby gc.CompanyName
-                        select gc;
-
-            var results = query.ToList();
-
-            GameCompaniesBox.ItemsSource = results;
-
-
-
-            //####################
-            #region oldTimerCode
-            //CountDownTimer timer = new CountDownTimer();
-
-
-            ////set to 30 mins
-            //timer.SetTime(30, 0);
-
-            //timer.Start();
-
-            ////update label text
-            //timer.TimeChanged += () => tblkSpecialTimer.Text = timer.TimeLeftMsStr;
-
-            //// show messageBox on timer = 00:00.000
-            //timer.CountDownFinished += () => MessageBox.Show("Timer finished the work!");
-
-            ////timer step. By default is 1 second
-            //timer.StepMs = 77; // for nice milliseconds time switch
-
-            #endregion oldTimerCode
-
-            DispatcherTimer dt = new DispatcherTimer();
-            dt.Interval = TimeSpan.FromSeconds(1);
-            dt.Tick += dt_Tick;
-            dt.Start();
-           
-        }
-
-        void dt_Tick(object sender, EventArgs e)
-        {
-            int minute = timeLimit.Minute;
-            int second = timeLimit.Second;
-            timeLimit = timeLimit.AddSeconds(-1);
-
-            tblkSpecialTimer.Text = timeLimit.ToString($"{minute} : {second}");
-        }
-        private void AddGamesAndGenres()
-        {
-
         }
 
         private void CbxGenre_SelectionChanged(object sender, SelectionChangedEventArgs e)
